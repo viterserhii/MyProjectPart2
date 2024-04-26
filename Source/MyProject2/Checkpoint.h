4147f5c971cdb70ed4768519203bd6444e1be23d
Serhii Viter <vitersergii@gmail.com>
2024-04-26 04:18:18 +0300

    Minor fix

--- Source/MyProject2/Checkpoint.h
+++ Source/MyProject2/Checkpoint.h
@@ -26,7 +26,7 @@ public:
     void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
     UFUNCTION(BlueprintImplementableEvent, Category = "Checkpoint")
-    void UpdateSpawnTransform(const FTransform& NewSpawnTransform);
+    void UpdateSpawnTransform(const FTransform NewSpawnTransform);
 
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
     USphereComponent* CollisionComponent;
@@ -51,9 +51,6 @@ public:
 
     UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Components")
     UMaterialInstanceDynamic* DynamicMaterialInstance;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-    USceneComponent* DefaultSceneRoot;
 
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
     UAudioComponent* AudioComponent; 
