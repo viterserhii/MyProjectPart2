2cd8f4d7ad06db214cbdf39de79c46a0bce3138e
Serhii Viter <vitersergii@gmail.com>
2024-04-27 07:35:28 +0300

    Add class 3

---
+++ Source/MyProject2/MeshMoverAlongSplineComponent.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "MeshMoverAlongSplineComponent.generated.h"
+
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class MYPROJECT2_API UMeshMoverAlongSplineComponent : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	//...
+};
